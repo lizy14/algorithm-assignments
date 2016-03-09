664dec629d4fa18aecfeed1a2bd66b5ec971834a
Li Zhaoyang <lizy14@yeah.net>
2016-03-09 01:31:16 +0000

    minor

--- week3/SortAlgorithms/main.cpp
+++ week3/SortAlgorithms/main.cpp
@@ -1,5 +1,8 @@
 /*
 Algorithms Assignment #3,
+
+Comparison of sorting algorithms Insertion, Merge, Quick and Radix
+
 By Zhaoyang, March 2016
 */
 
@@ -19,20 +22,25 @@ using namespace std;
 int main(){
 	Timing timer;
 	Vector v;
+#ifdef _DEBUG
 	v.randomize(10);
 	cout <<v << endl;
 	v.radixSort();
 	cout << v << endl;
-	
+#endif
 
-	int sizes[] = {10,1e2,1e3,1e4,1e5,1e6,1e7,1e8,2e8};
+	int sizes[] = {10,1e2,1e3,1e4,1e5,1e6,1e7,1e8,2e8,1e9};
 	cout << "size\tinsertion\tmerge\tquick\tradix\n";
 	for(auto size : sizes){
 		cout << size << '\t';
 		int totalTime, times;
 		
 		totalTime = 0, times = 0;
-		while(0){
+		while(1){
+			if(size >1e6){
+				cout << "TOO_LONG" << '\t';
+				break;
+			}
 			v.randomize(size);
 			timer.start();
 			v.insertionSort();
